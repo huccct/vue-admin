e8ae90c5f70bf6c01d1a1f94c04b40b5b600c5c7
huccct <87902123+huccct@users.noreply.github.com>
2023-05-28 02:57:19 +0000

    Deploying to gh-pages from @ huccct/vue-admin@4952f330599a2b1b36148e8ffbf884b7bf893afc 🚀

--- assets/index-3ef4ebb5.js
+++
@@ -1,1 +0,0 @@
-import{U as e,o as c,C as n}from"./index-324c4f35.js";const o={};function r(t,s){return c(),n("h1",null,"SKU管理")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ assets/index-726e4329.js
@@ -0,0 +1,1 @@
+import{U as e,o as c,C as n}from"./index-7d0dca2c.js";const o={};function r(t,s){return c(),n("h1",null,"数据大屏")}const _=e(o,[["render",r]]);export{_ as default};
