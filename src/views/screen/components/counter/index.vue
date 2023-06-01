ded9712e4fd8628ae0135e88a9e12dd1531da9d8
huccct <2972223145@qq.com>
2023-06-01 22:47:45 +0800

    feat(screen): screen模块完成

--- src/views/screen/components/counter/index.vue
+++ src/views/screen/components/counter/index.vue
@@ -2,27 +2,27 @@
   <div class="box8">
     <div class="title">
       <p>数据统计</p>
-      <img src="../../images/dataScreen-title.png" alt="">
+      <img src="../../images/dataScreen-title.png" alt="" />
     </div>
     <div class="charts" ref="charts"></div>
   </div>
 </template>
-  
+
 <script setup lang="ts">
-import * as echarts from 'echarts';
-import { ref, onMounted } from 'vue';
+import * as echarts from 'echarts'
+import { ref, onMounted } from 'vue'
 //获取DOM节点
-let charts = ref();
+let charts = ref()
 //组件挂载完毕
 onMounted(() => {
   //一个容器可以同时展示多种类型的图形图标
-  let mychart = echarts.init(charts.value);
+  let mychart = echarts.init(charts.value)
   let option = {
     title: {
       text: '游客消费统计',
-      textStyle:{
-        color:'white'
-      }
+      textStyle: {
+        color: 'white',
+      },
     },
     radar: {
       // shape: 'circle',
@@ -32,8 +32,8 @@ onMounted(() => {
         { name: '出行', max: 30000 },
         { name: '小吃', max: 38000 },
         { name: '爱好', max: 52000 },
-        { name: '景点', max: 25000 }
-      ]
+        { name: '景点', max: 25000 },
+      ],
     },
     series: [
       {
@@ -42,21 +42,21 @@ onMounted(() => {
         data: [
           {
             value: [4200, 3000, 20000, 35000, 50000, 18000],
-            name: '购物'
+            name: '购物',
           },
           {
             value: [5000, 14000, 28000, 26000, 42000, 21000],
-            name: '吃饭'
-          }
-        ]
-      }
-    ]
-  };
+            name: '吃饭',
+          },
+        ],
+      },
+    ],
+  }
   //设置配置项
   mychart.setOption(option)
 })
 </script>
-  
+
 <style scoped lang="scss">
 .box8 {
   width: 100%;
@@ -75,6 +75,5 @@ onMounted(() => {
   .charts {
     height: calc(100% - 30px);
   }
-
 }
 </style>
--- src/views/screen/components/year/index.vue
+++ src/views/screen/components/year/index.vue
@@ -2,46 +2,49 @@
   <div class="box7">
     <div class="title">
       <p>年度游客量对比</p>
-      <img src="../../images/dataScreen-title.png" alt="">
+      <img src="../../images/dataScreen-title.png" alt="" />
     </div>
     <div class="charts" ref="charts"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import * as echarts from 'echarts';
-import { ref, onMounted } from 'vue';
+import * as echarts from 'echarts'
+import { ref, onMounted } from 'vue'
 //获取DOM节点
-let charts = ref();
+let charts = ref()
 //组件挂载完毕
 onMounted(() => {
   //一个容器可以同时展示多种类型的图形图标
-  let mychart = echarts.init(charts.value);
+  let mychart = echarts.init(charts.value)
   //设置配置项
   mychart.setOption({
     title: {
       text: '散点图',
       left: '40%',
       textStyle: {
-        color: 'white'
-      }
+        color: 'white',
+      },
     },
     xAxis: {
       type: 'category',
       show: true,
     },
     yAxis: {
-      show: false
+      show: false,
     },
     grid: {
       left: 20,
       top: 20,
       right: 0,
-      bottom: 20
+      bottom: 20,
     },
     series: {
       type: 'scatter',
-      data: [33, 88, 21, 9, 88, 234, 113, 1231, 674, 3, 88, 33, 21, 888, 3332, 313, 123, 5, 657, 7],
+      data: [
+        33, 88, 21, 9, 88, 234, 113, 1231, 674, 3, 88, 33, 21, 888, 3332, 313,
+        123, 5, 657, 7,
+      ],
       //标记图形设置
       symbol: 'diamond',
       symbolSize: 16,
@@ -49,7 +52,7 @@ onMounted(() => {
       label: {
         show: true,
         position: 'top',
-        color: 'red'
+        color: 'red',
       },
       //散点图标记的颜色
       itemStyle: {
@@ -59,16 +62,20 @@ onMounted(() => {
           y: 0,
           x2: 0,
           y2: 1,
-          colorStops: [{
-            offset: 0, color: 'red' // 0% 处的颜色
-          }, {
-            offset: 1, color: 'blue' // 100% 处的颜色
-          }],
-          global: false // 缺省为 false
-        }
-      }
-    }
-
+          colorStops: [
+            {
+              offset: 0,
+              color: 'red', // 0% 处的颜色
+            },
+            {
+              offset: 1,
+              color: 'blue', // 100% 处的颜色
+            },
+          ],
+          global: false, // 缺省为 false
+        },
+      },
+    },
   })
 })
 </script>
@@ -91,6 +98,5 @@ onMounted(() => {
   .charts {
     height: calc(100% - 30px);
   }
-
 }
 </style>
