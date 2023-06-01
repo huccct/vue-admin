9bc736f41e9b5cb272cd6ce3642850ea68189e61
huccct <2972223145@qq.com>
2023-06-01 20:26:00 +0800

    feat(home): home模块完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,19 +3,65 @@
  * @Author: Huccct
  * @Date: 2023-05-19 17:38:54
  * @LastEditors: Huccct
- * @LastEditTime: 2023-05-24 10:19:00
+ * @LastEditTime: 2023-06-01 20:24:30
 -->
 <script setup lang="ts">
-import { onMounted } from 'vue'
-
 import useUserStore from '@/store/modules/user'
+import { getTime } from '@/utils/time'
 let userStore = useUserStore()
-
-onMounted(() => {
-  userStore.userInfo()
-})
 </script>
 <template>
-  <h1></h1>
+  <el-card>
+    <div class="box">
+      <img :src="userStore.avatar" alt="" class="avatar" />
+      <div class="footer">
+        <h3 class="title">
+          {{ getTime() }}好~
+          <span class="gradient">{{ userStore.username }}</span>
+        </h3>
+        <p class="subtitle">Vue-Admin</p>
+      </div>
+    </div>
+  </el-card>
+  <div class="bottom">
+    <def-svg-icon name="welcome" width="600px" height="300px"></def-svg-icon>
+  </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.box {
+  display: flex;
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+  .footer {
+    margin-left: 20px;
+    margin-top: 15px;
+    .title {
+      font-size: 35px;
+      margin-bottom: 30px;
+      font-weight: 900;
+      .gradient {
+        background: linear-gradient(to right, #001529, #001529, #ffffff);
+        /* 渐变方向是从左到右，颜色从红色到绿色 */
+        background-clip: text;
+        -webkit-background-clip: text; /* 兼容WebKit浏览器（例如Chrome和Safari） */
+        color: transparent;
+        font-size: 24px;
+        font-weight: bold;
+      }
+    }
+    .subtitle {
+      font-style: italic;
+      color: #ccc;
+      font-weight: 700;
+    }
+  }
+}
+.bottom {
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+}
+</style>
